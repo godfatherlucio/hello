6835835fb8b08833b2cc49948f5732550b31c1f0
Gage Witcig <gwitcig2@huskers.unl.edu>
2021-08-27 01:11:09 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ *
+ * Author: Gage Witcig
+ * Date: 2021/08/26
+ *
+ * A C Program that displays my name
+ *
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+  printf("Gage Witcig\n");
+
+  return 0;
+
+ }
